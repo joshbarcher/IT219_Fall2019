357972aad7851cc45d8b01c755c716aa95e61332
Josh Archer <jarcher@greenriver.edu>
2019-09-23 15:13:09 -0700

    Added a hello world program as an introduction to Java.

---
+++ My First Project/src/introduction/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+    This is a hello world program in Java!
+    Author: Josh Archer
+    Date: 9-23-2019
+    File: HelloWorld.java
+ */
+
+package introduction;
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        //send some output to the console
+        System.out.println("Hello, world!");
+    }
+}
